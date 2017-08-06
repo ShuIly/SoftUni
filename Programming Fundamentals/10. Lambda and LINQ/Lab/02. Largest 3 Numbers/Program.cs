078210497d994ccc2ed6c4699f3d00e5dc25615b
Shully <hacerqwett@gmail.com>
2017-08-06 18:58:40 +0300

    Added Lambda & LINQ Lab

---
+++ Programming Fundamentals/10. Lambda and LINQ/Lab/02. Largest 3 Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Largest_3_Numbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> arr = Console.ReadLine().Split().Select(int.Parse).ToList();
+			Console.WriteLine(string.Join(" ", arr.OrderByDescending(x => x).Take(3)));
+		}
+	}
+}
---
+++ Programming Fundamentals/10. Lambda and LINQ/Lab/03. Short Words Sorted/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Short_Words_Sorted
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> words = Console.ReadLine().ToLower()
+				.Split(new char[] { '.', ',', ':', ';', '(', ')', '[', ']', '"', '\'', '\\', '/', '!', '?', ' ' }, 
+				StringSplitOptions.RemoveEmptyEntries)
+				.Where(x => x.Length < 5).Distinct().OrderBy(x => x).ToList();
+			Console.WriteLine(string.Join(", ", words));
+		}
+	}
+}
