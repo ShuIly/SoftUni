8c44c2be7b8beff7498e03659042afa2b175e90f
Shully <hacerqwett@gmail.com>
2017-08-14 23:02:08 +0300

    Added File Exercises

--- Programming Fundamentals/12. Files Directories and Exceptions/Exercises/04. Re-Directory/Program.cs
+++ Programming Fundamentals/12. Files Directories and Exceptions/Exercises/04. Re-Directory/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,34 @@ namespace _04.Re_Directory
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] filePaths = Directory.GetFiles("input");
+			string lastExtention = null;
+
+			// Sort filepaths by extension
+			filePaths = filePaths.OrderBy(s => Path.GetExtension(s)).ToArray();
+
+			for (int i = 0; i < filePaths.Length; i++)
+			{
+				string extension = Path.GetExtension(filePaths[i]);
+
+				// If there's a file with no extention ignore it
+				if (string.IsNullOrEmpty(extension))
+				{
+					continue;
+				}
+
+				string dirName = extension.Substring(1);
+				string fileName = Path.GetFileName(filePaths[i]);
+
+				if (lastExtention != extension)
+				{
+					Directory.CreateDirectory($"input\\{dirName}s");
+					lastExtention = extension;
+				}
+
+				// Move file to the directory
+				File.Move(filePaths[i], $"input\\{dirName}s\\{fileName}");
+			}
 		}
 	}
 }
---
+++ Programming Fundamentals/12. Files Directories and Exceptions/Exercises/05. Products/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Products
+{
+	class Product
+	{
+
+		private static Dictionary<string, Product> products =
+			GetStockProducts();
+
+		private static Dictionary<string, Product> activeProducts =
+			new Dictionary<string, Product>();
+
+		private static HashSet<string> productTypes =
+			new HashSet<string>()
+			{
+				"Electronics",
+				"Food",
+				"Domestics"
+			};
+
+		public string Name { get; set; }
+		public string Type { get; set; }
+		public decimal Price { get; set; }
+		public int Quantity { get; set; }
+
+		public Product(string name, string type, decimal price, int quantity)
+		{
+			Name = name;
+			Type = type;
+			Price = price;
+			Quantity = quantity;
+		}
+
+		public static void AddProduct(string name, string type, decimal price, int quantity)
+		{
+			Product product = new Product(name, type, price, quantity);
+
+			// If product already exists just update its price and quantity
+			if (products.ContainsKey(name) && products[name].Type == type)
+			{
+				products[name] = product;
+			}
+			else
+			{
+				productTypes.Add(type);
+				activeProducts[name] = product;
+			}
+		}
+
+		public decimal GetSumPrice()
+		{
+			return Quantity * Price;
+		}
+
+		public void PrintProduct()
+		{
+			Console.WriteLine($"{Type}, Product: {Name}");
+			Console.WriteLine($"Price: ${Price}, Amount Left: {Quantity}");
+		}
+
+		private static Dictionary<string, Product> GetStockProducts()
+		{
+			Dictionary<string, Product> stockedProducts =
+				new Dictionary<string, Product>();
+
+			// In case database text file doesn't exist
+			if (!File.Exists("products-database.txt"))
+			{
+				File.Create("products-database.txt");
+				return stockedProducts;
+			}
+
+			using (StreamReader reader = new StreamReader("products-database.txt"))
+			{
+				while (!reader.EndOfStream)
+				{
+					string[] tokens = reader.ReadLine().Split();
+					if (tokens.Length != 4)
+					{
+						continue;
+					}
+					string name = tokens[0];
+					string type = tokens[1];
+					decimal price = decimal.Parse(tokens[2]);
+					int quantity = int.Parse(tokens[3]);
+
+					Product product = new Product(name, type, price, quantity);
+					stockedProducts.Add(name, product);
+				}
+			}
+
+			return stockedProducts;
+		}
+
+		public static void PrintStockedProducts()
+		{
+			Dictionary<string, Product> stockedProducts = GetStockProducts();
+
+			if (stockedProducts.Count > 0)
+			{
+				foreach (var product in stockedProducts
+					.OrderBy(p => p.Value.Type))
+				{
+					product.Value.PrintProduct();
+				}
+			}
+			else
+			{
+				Console.WriteLine("No products stocked");
+			}
+		}
+
+		public static void PrintSales()
+		{
+			Dictionary<string, decimal> typeSumDict =
+				new Dictionary<string, decimal>();
+
+			foreach (var type in productTypes)
+			{
+				decimal productTypeSum = 0;
+
+				foreach (var product in activeProducts
+					.Where(p => p.Value.Type == type))
+				{
+					productTypeSum += product.Value.GetSumPrice();
+				}
+
+				foreach (var product in products
+					.Where(p => p.Value.Type == type))
+				{
+					productTypeSum += product.Value.GetSumPrice();
+				}
+
+				typeSumDict.Add(type, productTypeSum);
+			}
+
+			foreach (var typeSum in typeSumDict
+				.OrderByDescending(s => s.Value))
+			{
+				string type = typeSum.Key;
+				decimal sum = typeSum.Value;
+
+				if (sum == 0)
+				{
+					Console.WriteLine("No products stocked");
+				}
+				else
+				{
+					Console.WriteLine($"{type}: ${sum:F2}");
+				}
+			}
+		}
+
+		public static void UpdateDatabase()
+		{
+			using (StreamWriter writer = new StreamWriter("products-database.txt"))
+			{
+				foreach (var product in products)
+				{
+					string name = product.Value.Name;
+					string type = product.Value.Type;
+					decimal price = product.Value.Price;
+					int quantity = product.Value.Quantity;
+
+					writer.WriteLine($"{name} {type} {price} {quantity}");
+				}
+
+				foreach (var product in activeProducts)
+				{
+					string name = product.Value.Name;
+					string type = product.Value.Type;
+					decimal price = product.Value.Price;
+					int quantity = product.Value.Quantity;
+
+					writer.WriteLine($"{name} {type} {price} {quantity}");
+				}
+			}
+		}
+	}
+
+	// Honestly the only problem I have with this is that there can
+	// be two objects with the same name even though their types are different.
+	// For example Banana with type Food and Banana with type Electronics (stupid, right?)
+	// Using a dictionary with a HashSet<Product> would solve that problem but
+	// it just feels so wrong. So I have omitted that.
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			while (input != "exit")
+			{
+				switch (input)
+				{
+					case "analyze":
+						Product.PrintStockedProducts();
+						break;
+					case "sales":
+						Product.PrintSales();
+						break;
+					case "stock":
+						Product.UpdateDatabase();
+						break;
+					default:
+						string[] inputTokens = input.Split();
+						string name = inputTokens[0];
+						string type = inputTokens[1];
+						decimal price = decimal.Parse(inputTokens[2]);
+						int quantity = int.Parse(inputTokens[3]);
+						Product.AddProduct(name, type, price, quantity);
+						break;
+				}
+
+				input = Console.ReadLine();
+			}
+		}
+	}
+}
