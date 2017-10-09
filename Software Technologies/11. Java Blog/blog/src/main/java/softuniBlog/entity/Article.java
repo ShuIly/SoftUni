5721efa92d950976c3f221f48acd7c0607666ad1
Shully <hacerqwett@gmail.com>
2017-10-09 20:42:08 +0300

    Worked on Java Blog

---
+++ Software Technologies/11. Java Blog/blog/src/main/java/softuniBlog/entity/Article.java
@@ -0,0 +1,48 @@
+package softuniBlog.entity;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "articles")
+public class Article {
+    private Integer id;
+
+    private String title;
+
+    private String content;
+
+    private User author;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+}
