92186eb9fddb7728a6c7aa6bf51e62ea285e7382
Shully <hacerqwett@gmail.com>
2017-10-14 23:28:25 +0300

    Finished C# ASP.NET

---
+++ Software Technologies/12. C# ASP.NET/02. TodoList/02. TodoList/Models/TaskDbContext.cs
@@ -0,0 +1,16 @@
+namespace _02.TodoList.Models
+{
+	using System;
+	using System.Data.Entity;
+	using System.Linq;
+
+	public class TaskDbContext : DbContext
+	{
+		public TaskDbContext()
+			: base("name=TaskDbContext")
+		{
+		}
+
+		public virtual DbSet<Task> Tasks { get; set; }
+	}
+}
