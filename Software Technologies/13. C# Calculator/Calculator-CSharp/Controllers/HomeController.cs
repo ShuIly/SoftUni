475f79eda2a956c84ed5b3593ad1a3c03b9334f6
Shully <hacerqwett@gmail.com>
2017-10-17 20:18:26 +0300

    Added C# Calculator

--- Software Technologies/13. C# Calculator/Calculator-CSharp/Controllers/HomeController.cs
+++ Software Technologies/13. C# Calculator/Calculator-CSharp/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
 ﻿using System.Web.Mvc;
+using Calculator_CSharp.Models;
 
 namespace Calculator_CSharp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(Calculator calculator)
         {
-            return View();
+            return View(calculator);
         }
+
+	    [HttpPost]
+	    public ActionResult Calculate(Calculator calculator)
+	    {
+		    calculator.Result = calculator.CalculateResult();
+
+		    return RedirectToAction("Index", calculator);
+	    }
     }
 }
---
+++ Software Technologies/13. C# Calculator/Calculator-CSharp/Models/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+
+namespace Calculator_CSharp.Models
+{
+	public class Calculator
+	{
+		public decimal LeftOperand { get; set; }
+
+		public decimal RightOperand { get; set; }
+
+		public string Operator { get; set; }
+
+		public decimal Result { get; set; }
+
+		public Calculator()
+		{
+			Result = 0;
+		}
+
+		public decimal CalculateResult()
+		{
+			decimal result = 0m;
+
+			switch (Operator)
+			{
+				case "+":
+					result = LeftOperand + RightOperand;
+					break;
+				case "-":
+					result = LeftOperand - RightOperand;
+					break;
+				case "*":
+					result = LeftOperand * RightOperand;
+					break;
+				case "/":
+					result = LeftOperand / RightOperand;
+					break;
+			}
+
+			return result;
+		}
+	}
+}
