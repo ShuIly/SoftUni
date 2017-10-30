80a492c4d45f1a42ed33183078aab6ddbec96541
Shully <hacerqwett@gmail.com>
2017-10-30 23:34:43 +0300

    Added Hashsets and Dictionaries Exercises (2/14)

---
+++ C# Advanced/02. Sets and Dictionaries/Exercises/01. Unique Usernames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        var usernames = new HashSet<string>();
+	        int n = int.Parse(Console.ReadLine());
+
+	        for (int i = 0; i < n; i++)
+	        {
+		        usernames.Add(Console.ReadLine());
+	        }
+
+	        Console.WriteLine(string.Join(Environment.NewLine, usernames));
+        }
+    }
+}
---
+++ C# Advanced/02. Sets and Dictionaries/Exercises/02. Sets of Elements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string[] inputTokens = Console.ReadLine()
+				.Split(new []{ ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+	        int n = int.Parse(inputTokens[0]);
+	        int m = int.Parse(inputTokens[1]);
+
+			var nums = new HashSet<int>();
+			var repeatableNums = new HashSet<int>();
+
+	        for (int i = 0; i < n; i++)
+	        {
+		        nums.Add(int.Parse(Console.ReadLine()));
+	        }
+			
+	        for (int i = 0; i < m; i++)
+	        {
+		        int num = int.Parse(Console.ReadLine());
+		        if (nums.Contains(num))
+			        repeatableNums.Add(num);
+	        }
+
+	        Console.WriteLine(string.Join(" ", repeatableNums));
+        }
+    }
+}
