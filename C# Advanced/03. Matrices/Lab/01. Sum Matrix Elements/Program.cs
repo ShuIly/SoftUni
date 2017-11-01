675de44f5bcb63f45a3616afdc463ac59de96459
Shully <hacerqwett@gmail.com>
2017-11-01 22:19:52 +0200

    Added Matrices Lab

---
+++ C# Advanced/03. Matrices/Lab/01. Sum Matrix Elements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] colRow = Console.ReadLine()
+				.Split(new []{ ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+
+	        int col = colRow[0];
+
+			int[][] matrix = new int[col][];
+	        for (int i = 0; i < col; i++)
+	        {
+				matrix[i] = Console.ReadLine()
+					.Split(new []{ ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+					.Select(int.Parse)
+					.ToArray();
+	        }
+
+	        int sumMatrix = 0;
+	        foreach (var row in matrix)
+		        sumMatrix += row.Sum();
+
+		    string result = $"{colRow[0]}\n{colRow[1]}\n{sumMatrix}\n";
+	        Console.WriteLine(result);
+        }
+    }
+}
