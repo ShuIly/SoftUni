ff0f77e3d16dd647be81f8a93794231110c16d1c
Shully <hacerqwett@gmail.com>
2017-11-06 14:06:19 +0200

    Finished Exam Oct 2017

---
+++ Programming Fundamentals Extended/Exams/Programming Fundamentals Exam - 11 Oct 2017/03. Anonymous Vox/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03.Anonymous_Vox
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string encodedRegex = @"([A-Za-z]+)(?<placeholder>.+)\1";
+			string input = Console.ReadLine();
+			Queue<string> placeholders = new Queue<string>
+				(Console.ReadLine().Split(new[] { '{', '}' }, StringSplitOptions.RemoveEmptyEntries));
+
+			string result = input;
+
+			MatchCollection matches = Regex.Matches(input, encodedRegex);
+			int count = Math.Min(matches.Count, placeholders.Count);
+
+			int indexDiff = 0;
+			for (int i = 0; i < count; i++)
+			{
+				string matchPlaceholder = matches[i].Groups["placeholder"].Value;
+				int matchPlaceholderIndex = matches[i].Groups["placeholder"].Index;
+				int placeholderLength = placeholders.Peek().Length;
+
+				result = result.Substring(0, matchPlaceholderIndex - indexDiff)
+						 + placeholders.Dequeue()
+						 + input.Substring(matchPlaceholderIndex + matchPlaceholder.Length);
+
+				indexDiff += matchPlaceholder.Length - placeholderLength;
+			}
+
+			Console.WriteLine(result);
+		}
+	}
+}
---
+++ Programming Fundamentals Extended/Exams/Programming Fundamentals Exam - 11 Oct 2017/04. Anonymous Cache/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Anonymous_Cache
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var datasets = new Dictionary<string, Dictionary<string, long>>();
+			var cache = new Dictionary<string, Dictionary<string, long>>();
+
+			while (true)
+			{
+				string input = Console.ReadLine();
+				if (input == "thetinggoesskrra")
+					break;
+
+				string[] inputTokens = input
+					.Split(new[] { ' ', '-', '>', '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+				if (inputTokens.Length == 1)
+				{
+					if (!datasets.ContainsKey(inputTokens[0]))
+					{
+						datasets.Add(inputTokens[0], new Dictionary<string, long>());
+						if (cache.ContainsKey(inputTokens[0]))
+						{
+							foreach (var cacheSet in cache[inputTokens[0]])
+								datasets[inputTokens[0]].Add(cacheSet.Key, cacheSet.Value);
+
+							cache.Remove(inputTokens[0]);
+						}
+					}
+
+					continue;
+				}
+
+				string datakey = inputTokens[0];
+				long datasize = long.Parse(inputTokens[1]);
+				string dataset = inputTokens[2];
+
+				if (!datasets.ContainsKey(dataset))
+				{
+					if (!cache.ContainsKey(dataset))
+						cache.Add(dataset, new Dictionary<string, long>());
+					cache[dataset].Add(datakey, datasize);
+				}
+				else
+					datasets[dataset].Add(datakey, datasize);
+			}
+
+			if (datasets.Count > 0)
+			{
+				var bestDataset = datasets
+					.OrderByDescending(d => d.Value.Sum(s => s.Value))
+					.First();
+
+				Console.WriteLine($"Data Set: {bestDataset.Key}, Total Size: {bestDataset.Value.Sum(s => s.Value)}");
+				foreach (var datakey in bestDataset.Value)
+					Console.WriteLine($"$.{datakey.Key}");
+			}
+		}
+	}
+}
