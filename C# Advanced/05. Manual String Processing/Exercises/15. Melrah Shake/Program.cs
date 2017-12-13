cec22b4d55aa84f610142f9139c886760cd5e73c
Shully <hacerqwett@gmail.com>
2017-12-13 16:25:16 +0200

    Finished Manual String Processing Exercises

--- C# Advanced/05. Manual String Processing/Exercises/15. Melrah Shake/Program.cs
+++ C# Advanced/05. Manual String Processing/Exercises/15. Melrah Shake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace _15.Melrah_Shake
 {
@@ -6,6 +7,34 @@ namespace _15.Melrah_Shake
     {
         static void Main(string[] args)
         {
+	        StringBuilder strSb = new StringBuilder(Console.ReadLine());
+	        StringBuilder patternSb = new StringBuilder(Console.ReadLine());
+
+	        while (patternSb.Length != 0)
+	        {
+		        string str = strSb.ToString(); 
+		        string pattern = patternSb.ToString(); 
+
+		        int indexStart = str.IndexOf(pattern);
+		        if (indexStart == -1)
+		        {
+			        break;
+		        }
+
+		        int indexEnd = str.LastIndexOf(pattern);
+
+		        if (indexStart != indexEnd)
+		        {
+			        strSb = strSb.Remove(indexEnd, patternSb.Length)
+				        .Remove(indexStart, patternSb.Length);
+
+			        Console.WriteLine("Shaked it.");
+		        }
+
+		        patternSb = patternSb.Remove(patternSb.Length / 2, 1);
+	        }
+
+	        Console.WriteLine("No shake.\n" + strSb);
         }
     }
 }
