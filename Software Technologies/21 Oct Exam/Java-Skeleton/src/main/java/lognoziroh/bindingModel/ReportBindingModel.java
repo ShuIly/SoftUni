617cf14f063819842a3c44294fe3e2c8cb679116
Shully <hacerqwett@gmail.com>
2017-12-22 20:46:40 +0200

    Added Oct Inst Exam

---
+++ Software Technologies/21 Oct Exam/Java-Skeleton/src/main/java/lognoziroh/bindingModel/ReportBindingModel.java
@@ -0,0 +1,45 @@
+package lognoziroh.bindingModel;
+
+public class ReportBindingModel {
+    private Integer id;
+
+    private String status;
+
+    private String message;
+
+    private String origin;
+
+    public ReportBindingModel(){};
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+}
