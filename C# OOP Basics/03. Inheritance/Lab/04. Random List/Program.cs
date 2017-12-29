d86e930663bcc2448aeb625d4e8a6d1be6c5ca7c
Shully <hacerqwett@gmail.com>
2017-12-29 10:48:21 +0200

    Finished Inheritance Lab

---
+++ C# OOP Basics/03. Inheritance/Lab/04. Random List/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RandomList randomList = new RandomList();
+        randomList.Add("123");
+    }
+}
---
+++ C# OOP Basics/03. Inheritance/Lab/04. Random List/RandomList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+class RandomList : ArrayList
+{
+    public string RandomString()
+    {
+        int index = new Random().Next(0, this.Count - 1);
+
+        string element = this[index].ToString();
+
+        this.Remove(this[index]);
+
+        return element;
+    }
+}
