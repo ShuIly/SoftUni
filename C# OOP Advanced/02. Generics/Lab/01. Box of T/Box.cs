9939376408ebc314c56bff732e9bd06307e25600
Shully <hacerqwett@gmail.com>
2018-01-11 19:30:34 +0200

    Added Generics Lab

---
+++ C# OOP Advanced/02. Generics/Lab/01. Box of T/Box.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class Box<T> : Stack<T>
+{
+    public void Add(T item)
+    {
+        this.Push(item);
+    }
+
+    public T Remove()
+    {
+        return this.Pop();
+    }
+}
---
+++ C# OOP Advanced/02. Generics/Lab/03. Generic Scale/Scale.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Scale<T>
+    where T : IComparable<T>
+{
+    private readonly T left;
+    private readonly T right;
+
+    public Scale(T left, T right)
+    {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T GetHeavier()
+    {
+        int comparator = this.left.CompareTo(this.right);
+
+        if (comparator == 0)
+        {
+            return default(T);
+        }
+
+        return comparator > 0 ? this.left : this.right;
+    }
+}
