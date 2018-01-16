b781459c6ef1ace0e16896f863171037d1c86498
Shully <hacerqwett@gmail.com>
2018-01-16 16:49:19 +0200

    Finished Generics Exercises

--- C# OOP Advanced/02. Generics/Exercises/11. Threeuple/Program.cs
+++ C# OOP Advanced/02. Generics/Exercises/11. Threeuple/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 class Program
 {
@@ -14,9 +13,9 @@ class Program
         string[] thirdInput = Console.ReadLine()
             .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
 
-        var tuple1 = new Thre<string, string>($"{firstInput[0]} {firstInput[1]}", firstInput[2]);
-        var tuple2 = new Tuple<string, int>(secondInput[0], int.Parse(secondInput[1]));
-        var tuple3 = new Tuple<int, double>(int.Parse(thirdInput[0]), double.Parse(thirdInput[1]));
+        var tuple1 = new Threeuple<string, string, string>($"{firstInput[0]} {firstInput[1]}", firstInput[2], firstInput[3]);
+        var tuple2 = new Threeuple<string, int, bool>(secondInput[0], int.Parse(secondInput[1]), secondInput[2] == "drunk");
+        var tuple3 = new Threeuple<string, double, string>(thirdInput[0], double.Parse(thirdInput[1]), thirdInput[2]);
 
         Console.WriteLine(tuple1 + "\n" + tuple2 + "\n" + tuple3);
     }
